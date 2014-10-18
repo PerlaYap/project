6cfc806b77f2313f02fda298dccac37f9ce3dd43
lecdado <lyka.dado@yahoo.com>
2014-10-19 01:43:07 +0800

    revised audit trail

--- application/views/records/audittrail.php
+++ application/views/records/audittrail.php
@@ -1,8 +1,8 @@
 <TITLE> Audit Trail</TITLE>
 
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('Assets/css/reports.css'); ?>">
+<script src="<?php echo base_url('Assets/js/general.js'); ?>"></script>
 
-  <!-- <link rel="stylesheet" type="text/css" href="../../../Assets/css/reports.css">   -->
 
 
 <body>
@@ -13,199 +13,118 @@
 	
 	<h3>
 		CARITAS SALVE CREDIT COOPERATIVE <br> 
-		AUDIT TRAIL OF TAYUMAN BRANCH <br> 
-		FROM JANUARY 2014 TO DECEMBER 2014 <br>
+		AUDIT TRAIL<br> 
 	</h3>
+
+	<br>
+
+	<style type="text/css">
+		p.filter{
+			font-size: 14px;
+			margin-left: 145px;
+			
+		}
+
+		select.filtercategory, #filtername, #filterposition{
+			background: #FFF url('down-arrow.png') no-repeat right;
+		    background: #FFF url('down-arrow.png') no-repeat right);
+		    appearance:none;
+		    -webkit-appearance:none;
+		    -moz-appearance: none;
+		    text-indent: 0.01px;
+		    text-overflow: '';
+		    width: 100px;
+		    height: 25px;
+		}
+
+		.date{
+			position: absolute;
+			margin-top: -40px;
+			margin-left: 310px;
+			border:0;			
+		}
+
+		.filterbtn{
+			margin-top: -36px;
+			margin-left: 210px;
+		}
+
+		.user{
+			position: absolute;
+			margin-top: -42px;
+			margin-left: 310px;
+			border:0;
+		}
+
+		#filterposition{
+			width: 150px;
+		}
+
+		#filtername{
+			width: auto;
+
+		}
+
+	</style>
+
+	<p class="filter">FILTER: 
+		<select class="filtercategory" onchange="changeCategory(this.value)">
+			<option selected value="x">Category</option>
+			<option value="date">Date</option>
+			<option value="user">User</option>
+		</select>
+
+		<div id="date" class="date" style="display: none;">
+			<input type="text" style="width: 200px; height: 25px;"/>
+			<input type="button" value="Go" class="filterbtn" style="height: 28px;"/>
+		</div>
+
+
+
+		<div id="user" class="user" style="display: none;" onchange="enableName(this.value)">
+			<select id="filterposition">
+				<option selected value="x">Position</option>
+				<option value="so">Salve Officer</option>
+				<option value="bm">Branch Manager</option>
+			</select>
+
+			<select id="filtername" disabled>
+				<option selected value="x">Name</option>
+				<option value="a">Lyka Ellace Centino Dado</option>
+			</select>
+			<input type="button" value="Go"  style="height: 28px;"/>
+			
+		</div>
+
+		
+
+	</p> 
 
 	<br>
 
     <table class="dailycollectionsheet" border="1" style="margin-left:auto; margin-right: auto;">
 		<tr class="header">
 			<td class="num"><b>#</b></td>
-			<td style="width: 320px;"><b>ACTIVITY</b></td>
-			<td style="width: 180px;"><b>DATE</b></td>
-			<td style="width: 100px;"><b>TIME</b></td>
 			<td style="width: 220px;"><b>NAME</b></td>
 			<td style="width: 150px;"><b>POSITION</b></td>
+			<td style="width: 180px;"><b>DATE</b></td>
+			<td style="width: 100px;"><b>TIME</b></td>
+			<td style="width: 320px;"><b>ACTIVITY</b></td>
 		</tr>
 
 		<tr>
 			<td>1</td>
-			<td style="text-align: left;">Log in</td>
+			<td>Lyka Dado</td>
+			<td>Salve Officer</td>
 			<td>01/20/2014</td>
 			<td>12:22pm</td>
-			<td>Lyka Dado</td>
-			<td>Salve Officer</td>
+			<td style="text-align: left;">Log in</td>
+			
+			
+			
 		</tr>
 
-		<tr>
-			<td>2</td>
-			<td style="text-align: left;">Filed Membership Application</td>
-			<td>01/20/2014</td>
-			<td>1:00pm</td>
-			<td>Lyka Dado</td>
-			<td>Salve Officer</td>
-		</tr>
-		<tr>
-			<td>3</td>
-			<td style="text-align: left;">Log out</td>
-			<td>01/20/2014</td>
-			<td>2:38pm</td>
-			<td>Lyka Dado</td>
-			<td>Salve Officer</td>
-		</tr>
-		<tr>
-			<td>4</td>
-			<td style="text-align: left;">Log in</td>
-			<td>01/24/2014</td>
-			<td>9:10am</td>
-			<td>Perla Yap</td>
-			<td>Branch Manager</td>
-		</tr>
-		<tr>
-			<td>5</td>
-			<td style="text-align: left;">Approved Loan Application of Vergilia Ngo Uy</td>
-			<td>01/24/2014</td>
-			<td>9:30am</td>
-			<td>Perla Yap</td>
-			<td>Branch Manager</td>
-		</tr>
-		<tr>
-			<td>6</td>
-			<td style="text-align: left;">Approved Member Application of Alaiza Cajilo Tiu</td>
-			<td>01/24/2014</td>
-			<td>10:31am</td>
-			<td>Perla Yap</td>
-			<td>Branch Manager</td>
-		</tr>
-		<tr>
-			<td>7</td>
-			<td style="text-align: left;">Rejected Member Application of Oliver Sim Laigo</td>
-			<td>01/24/2014</td>
-			<td>10:52am</td>
-			<td>Perla Yap</td>
-			<td>Branch Manager</td>
-		</tr>
-		<tr>
-			<td>8</td>
-			<td style="text-align: left;">Log out</td>
-			<td>01/24/2014</td>
-			<td>11:20am</td>
-			<td>Perla Yap</td>
-			<td>Branch Manager</td>
-		</tr>
-		<tr>
-			<td>9</td>
-			<td style="text-align: left;">Log in</td>
-			<td>01/28/2014</td>
-			<td>3:45pm</td>
-			<td>Sheila Uy</td>
-			<td>Salve Officer</td>
-		</tr>
-		<tr>
-			<td>10</td>
-			<td style="text-align: left;">Daily collection for Center 2</td>
-			<td>01/28/2014</td>
-			<td>4:00pm</td>
-			<td>Sheila Uy</td>
-			<td>Salve Officer</td>
-		</tr>
-		<tr>
-			<td>11</td>
-			<td style="text-align: left;">Individual Collection for Angela Tiu Yap</td>
-			<td>01/28/2014</td>
-			<td>5:17pm</td>
-			<td>Sheila Uy</td>
-			<td>Salve Officer</td>
-		</tr>
-		<tr>
-			<td>12</td>
-			<td style="text-align: left;">Log out</td>
-			<td>01/28/2014</td>
-			<td>6:00pm</td>
-			<td>Sheila Uy</td>
-			<td>Salve Officer</td>
-		</tr>
-		<tr>
-			<td>13</td>
-			<td style="text-align: left;">Log in</td>
-			<td>02/10/2014</td>
-			<td>5:30pm</td>
-			<td>Kendrick Lim</td>
-			<td>Admin</td>
-		</tr>
-		<tr>
-			<td>14</td>
-			<td style="text-align: left;">Added New Officer Marion Meneses</td>
-			<td>02/10/2014</td>
-			<td>4:30pm</td>
-			<td>Kendrick Lim</td>
-			<td>Admin</td>
-		</tr>
-		<tr>
-			<td>15</td>
-			<td style="text-align: left;">Reset password of John Villaflor Ho</td>
-			<td>02/10/2014</td>
-			<td>5:12pm</td>
-			<td>Kendrick Lim</td>
-			<td>Admin</td>
-		</tr>
-		<tr>
-			<td>16</td>
-			<td style="text-align: left;">Log out</td>
-			<td>01/28/2014</td>
-			<td>5:51pm</td>
-			<td>Kendrick Lum</td>
-			<td>Admin</td>
-		</tr>
-		<tr>
-			<td>17</td>
-			<td style="text-align: left;">Log in</td>
-			<td>01/28/2014</td>
-			<td>4:00pm</td>
-			<td>Marion Meneses</td>
-			<td>Salve Officer</td>
-		</tr>
-		<tr>
-			<td>18</td>
-			<td style="text-align: left;">Individual Collection for Angela Tiu Yap</td>
-			<td>01/28/2014</td>
-			<td>5:17pm</td>
-			<td>Marion Meneses</td>
-			<td>Salve Officer</td>
-		</tr>
-		<tr>
-			<td>19</td>
-			<td style="text-align: left;">Log out</td>
-			<td>01/28/2014</td>
-			<td>6:00pm</td>
-			<td>Marion Meneses</td>
-			<td>Salve Officer</td>
-		</tr>
-		<tr>
-			<td>20</td>
-			<td style="text-align: left;">Log in</td>
-			<td>02/10/2014</td>
-			<td>5:30pm</td>
-			<td>Perla Yap</td>
-			<td>Branch Manager</td>
-		</tr>
-		<tr>
-			<td>21</td>
-			<td style="text-align: left;">Released Loan of Vergilia Ngo Uy</td>
-			<td>02/10/2014</td>
-			<td>4:30pm</td>
-			<td>Perla Yap</td>
-			<td>Branch Manager</td>
-		</tr>
-		<tr>
-			<td>22</td>
-			<td style="text-align: left;">Log out</td>
-			<td>02/10/2014</td>
-			<td>5:12pm</td>
-			<td>Perla Yap</td>
-			<td>Branch Manager</td>
-		</tr>
+		
 
 	</table>
 
@@ -213,7 +132,7 @@
     
 
     <br><br>
-	<div style="width: 100%; text-align: center;">
+	<!--<div style="width: 100%; text-align: center;">
 		<button onclick="window.print()">Print</button> 
-	</div>
+	</div>-->
 </body>
