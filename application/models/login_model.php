52c798f7b5c55022f780000ea47a4d6e6c38730a
killercl <lim.kendrick28@gmail.com>
2014-10-18 23:19:11 +0800

    Minor errpor

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -40,11 +40,11 @@ where branch.ControlNo = cp.CaritasBranch_ControlNo AND cp.CaritasPersonnel_Cont
 		  if ($d>=6){
 
 		  		//$this->db->query("UPDATE `members_has_membersmembershipstatus` SET `Status` = 'Dormant Saver', `DateUpdated` = now() WHERE `ControlNo` ='$m'");
-		  		$this->addMembershipStatus($ControlNo,"Dormant Saver");
+		  		$this->addMembershipStatus($m,"Dormant Saver");
 
 		  }else {
-		  		$this->db->query("UPDATE `members_has_membersmembershipstatus` SET `Status` = 'Active Saver', `DateUpdated` = now() WHERE `ControlNo` ='$m'");
-		  		$this->addMembershipStatus($ControlNo,"Active Saver");
+		  		//$this->db->query("UPDATE `members_has_membersmembershipstatus` SET `Status` = 'Active Saver', `DateUpdated` = now() WHERE `ControlNo` ='$m'");
+		  		$this->addMembershipStatus($m,"Active Saver");
 		  }
 
 		  }
@@ -65,10 +65,10 @@ where branch.ControlNo = cp.CaritasBranch_ControlNo AND cp.CaritasPersonnel_Cont
 		  if ($p>=6){
 
 		  		//$this->db->query("UPDATE `members_has_membersmembershipstatus` SET `Status` = 'Past Due', `DateUpdated` = now() WHERE `ControlNo` ='$m2'");
-		  			$this->addMembershipStatus($ControlNo,"Past Due");
+		  			$this->addMembershipStatus($m,"Past Due");
 		  }else {
 		  		//$this->db->query("UPDATE `members_has_membersmembershipstatus` SET `Status` = 'Borrower', `DateUpdated` = now()WHERE `ControlNo` ='$m2'");
-		  		$this->addMembershipStatus($ControlNo,"Borrower");
+		  		$this->addMembershipStatus($m,"Borrower");
 		  }
 
 		  }
