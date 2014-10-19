8b919c001f125a6dcd98cf44a07470d17ddf57c3
killercl <lim.kendrick28@gmail.com>
2014-10-19 13:32:52 +0800

    Login

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -13,77 +13,87 @@ class Login_model extends CI_Model{
 		//grab user input
 
 		 $username = $this->security->xss_clean($this->input->post('username'));
-		 $password = $this->security->xss_clean($this->input->post('password'));	
-		$query1 = $this->db->query("Select branch.ControlNo as branchno, branch.BranchName, personnel.ControlNo as personnelno, personnel.FirstName, personnel.MiddleName, personnel.LastName, personnel.Rank, users.Username, users.Password
-from caritasbranch branch , caritaspersonnel personnel, caritasbranch_has_caritaspersonnel cp , users
-where branch.ControlNo = cp.CaritasBranch_ControlNo AND cp.CaritasPersonnel_ControlNo = personnel.ControlNo AND personnel.ControlNo = users.ControlNo AND users.IsActive = 1 ");
-
-		$q = $query1->result();
+		 $password = $this->security->xss_clean($this->input->post('password'));
+/*
+		$q = $logger->result();
 		 $size = count($q);
 		 $i=0;
-		 if ($query1->num_rows()>0) {
+		 if ($logger->num_rows()>0) {
 		 	while ($i<$size) {
-		 		$row = $query1->row_array($i);
-		 		if ($row['Username'] == $username && $row['Password']== $password) {
+		 		$row = $logger->row_array($i);
+		 		if ($row['Username'] == $username && $row['Password']== $password) { */
+		 		$logger=$this->db->query("SELECT branch.ControlNo as branchno, branch.BranchName, personnel.ControlNo as personnelno, personnel.FirstName, personnel.MiddleName, personnel.LastName, personnel.Rank, users.Username, users.Password
+													FROM caritasbranch branch , caritaspersonnel personnel, caritasbranch_has_caritaspersonnel cp , users
+													WHERE branch.ControlNo = cp.CaritasBranch_ControlNo AND cp.CaritasPersonnel_ControlNo = personnel.ControlNo AND personnel.ControlNo = users.ControlNo AND users.IsActive = 1 AND Username='$username' AND Password='$password'");
+				
+		 		
+				if($logger->num_rows()!=0){
 
-		 $member = $this->db->query("SELECT mm.ControlNo FROM transaction t, members_has_membersmembershipstatus mm WHERE t.Members_ControlNo  = mm.ControlNo AND t.transactiontype = 'Savings'");
+		 			$membersLoanChecker=$this->db->query("SELECT mem.ControlNo AS MemberControl, IFNULL(UNO.Status,NULL) AS LoanStatus, DateApplied, DOS.Status AS MembershipStatus, DateUpdated FROM Members mem
+										LEFT JOIN (SELECT Members_ControlNo, Status, DateApplied 
+										FROM loanapplication_has_members lhm 
+										LEFT JOIN LoanApplication la ON lhm.LoanApplication_ControlNo=la.ControlNo
+										GROUP BY Members_ControlNo ORDER BY Members_ControlNo ASC, DateApplied DESC)UNO
+										ON UNO.Members_ControlNo=mem.ControlNo
+										LEFT JOIN 
+										(SELECT mem.ControlNo, UNO.DateUpdated, UNO.Status FROM members mem
+										LEFT JOIN (SELECT * FROM members_has_membersmembershipstatus ORDER BY DateUpdated DESC)UNO ON
+										UNO.ControlNo=mem.ControlNo GROUP BY ControlNo) DOS
+										ON mem.ControlNo=DOS.ControlNo
+										WHERE Approved='YES'");
+		 			
+		 			foreach($membersLoanChecker->result() as $data){
 
-		 	foreach ($member->result() as $mem) {
-		  		$m = $mem->ControlNo;
+		 				if($data->LoanStatus =="Full Payment" || $data->LoanStatus=="NULL"){
+		 					$member = $this->db->query("SELECT TIMESTAMPDIFF(month, 
+														(SELECT DateTime
+														FROM 
+														(SELECT * FROM Transaction 
+														WHERE Members_ControlNo='$data->MemberControl' AND TransactionType='Savings' 
+														ORDER BY DateTime DESC)UNO 
+														GROUP BY Members_ControlNo  ), now()) as time ");
 
-		  $dormant = $this->db->query("SELECT TIMESTAMPDIFF(month, (SELECT max(t.DateTime) FROM transaction t WHERE t.Members_ControlNo = '$m' AND t.transactiontype = 'Savings' ), now()) as time ");
+		 					foreach($member->result() as $number){
+		 						$d=$number->time;
+		 					}
 
-		  	foreach ($dormant->result() as $do) {
-		  		$d = $do->time;
+		 					if ($d>=6){
+		 						if($data->MembershipStatus!="Dormant Saver")
+									$this->addMembershipStatus($data->MemberControl,"Dormant Saver");
+						  	}else {
+						  		if($data->MembershipStatus!="Active Saver")
+						  			$this->addMembershipStatus($data->MemberControl,"Active Saver");
+						  	}
+		 				}
 
+		 				else{
+		 					$member = $this->db->query("SELECT TIMESTAMPDIFF(month, 
+														(SELECT DateTime
+														FROM 
+														(SELECT * FROM Transaction 
+														WHERE Members_ControlNo='$data->MemberControl' AND (TransactionType='Loan' OR TransactionType='Past Due') AND Amount>0
+														ORDER BY DateTime DESC)UNO 
+														GROUP BY Members_ControlNo ), now()) as time ");
+		 					
+		 					foreach($member->result() as $number){
+		 						$d=$number->time;
+		 					}
 
-		  if ($d>=6){
+		 					if ($d>=6){
+		 						if($data->MembershipStatus!="Past Due")
+									$this->addMembershipStatus($data->MemberControl,"Past Due");
+						  	}else {
+						  		if(!$data->MembershipStatus!="Borrower")
+						  			$this->addMembershipStatus($data->MemberControl,"Borrower");
+						  	}
 
-		  		$this->db->query("UPDATE `members_has_membersmembershipstatus` SET `Status` = 'Dormant Saver', `DateUpdated` = now() WHERE `ControlNo` ='$m'");
-		  		//$this->addMembershipStatus($m,"Dormant Saver");
-
-		  }else {
-		  		$this->db->query("UPDATE `members_has_membersmembershipstatus` SET `Status` = 'Active Saver', `DateUpdated` = now() WHERE `ControlNo` ='$m'");
-		  		//$this->addMembershipStatus($m,"Active Saver");
-		  }
-
-		  }
-
-		}
-
-	$member2 = $this->db->query("SELECT mm.ControlNo FROM transaction t, members_has_membersmembershipstatus mm WHERE t.Members_ControlNo  = mm.ControlNo AND t.transactiontype = 'Loan'");
-
-		 	foreach ($member2->result() as $mem) {
-		  		$m2 = $mem->ControlNo;
-
-		  $pdm = $this->db->query("SELECT TIMESTAMPDIFF(month, (SELECT max(t.DateTime) FROM transaction t WHERE t.Members_ControlNo = '$m2' AND t.transactiontype = 'Loan' ), now()) as time ");
-
-		  	foreach ($pdm->result() as $do) {
-		  		$p = $do->time;
-
-
-		  if ($p>=6){
-
-		  		$this->db->query("UPDATE `members_has_membersmembershipstatus` SET `Status` = 'Past Due', `DateUpdated` = now() WHERE `ControlNo` ='$m2'");
-		  		//$this->addMembershipStatus($m,"Past Due");
-		  }else {
-		  		$this->db->query("UPDATE `members_has_membersmembershipstatus` SET `Status` = 'Borrower', `DateUpdated` = now()WHERE `ControlNo` ='$m2'");
-		  		//$this->addMembershipStatus($m,"Borrower");
-		  }
-
-		  }
-
-		}
-		  		
-		  	
-	
-		 			return $row;
+		 				}
+		 			}
 
 		 		}
-		 		$i++;	
+		 			return $logger->row_array(0);
 		 	}
-		 }	 
-	}
+
 
 	public function addMembershipStatus($ControlNo,$Status){
     $this->db->query("INSERT INTO `microfinance2`.`members_has_membersmembershipstatus` (`ControlNo`, `DateUpdated`, `Status`) VALUES ('$ControlNo', now() , '$Status');");
