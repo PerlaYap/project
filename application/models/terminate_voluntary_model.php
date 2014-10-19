bae81156fe40cd598c4a57a5c31aea58c1e50a8f
PerlaYap <573985431perla@gmail.com>
2014-10-19 09:20:39 +0800

    termination partial

---
+++ application/models/terminate_voluntary_model.php
@@ -0,0 +1,58 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class terminate_voluntary_model extends CI_Model{
+
+
+	function _construct(){
+
+		parent::_construct();
+	}
+
+	public function getcontrolno(){
+
+		 $controlno = $this->security->xss_clean($this->input->post('controlno'));
+		 //this is to be send
+		/* $profileinfo = $this->getprofileinfo($controlno);
+		 $branchcenter = $this->getbranchcenter($controlno);*/
+		 return $controlno;
+		 /*return $profileinfo->result();*/
+		 
+		 
+
+		}
+	public function getprofileinfo(){
+		$control_no = $this->getcontrolno();
+		$profileinfo = $this->db->query("SELECT  mem.ControlNo, MemberID,memname.LastName,memname.FirstName, memname.MiddleName, ContactNo,Birthday,BirthPlace, GenderID, Religion, Date(cm.DateEntered) as date, EducationalAttainment,CivilStatus, MFI,Address,AddressDate, Status, Type,BusinessType,CompanyName,CompanyContact,YearEntered, LoanExpense,Savings, CapitalShare 
+                                FROM 
+                                Members mem 
+                                LEFT JOIN MembersName memname ON mem.ControlNo=memname.ControlNo
+                                LEFT JOIN (SELECT ControlNo, Status FROM members_has_membersmembershipstatus ORDER BY DateUpdated DESC LIMIT 1) mhmstatus ON mem.ControlNo=mhmstatus.ControlNo
+                                
+                                LEFT JOIN (SELECT ControlNo, Type FROM members_has_membertype ORDER BY DateUpdated DESC LIMIT 1) mhmtype ON mem.ControlNo=mhmtype.ControlNo
+                                LEFT JOIN MembersContact memcontact ON mem.ControlNo=memcontact.ControlNo
+                                LEFT JOIN membersmfi mfi ON mem.ControlNo=mfi.ControlNo
+                                LEFT JOIN membersaddress madd ON mem.ControlNo=madd.ControlNo
+                                LEFT JOIN sourceofincome soi ON mem.ControlNo=soi.ControlNo
+                                LEFT JOIN CaritasCenters_has_Members cm ON mem.ControlNo = cm.Members_ControlNo
+                                WHERE mem.ControlNo='$control_no'");
+		
+		return $profileinfo->result();
+	}
+	public function getbranchcenter(){
+		$control_no = $this->getcontrolno();
+		$branchcenter = $this->db->query("SELECT cm.`CaritasCenters_ControlNo`, cc.`CenterNo`, cbc.`CaritasBranch_ControlNo`, b.`BranchName`
+							FROM `caritascenters_has_members` cm, `caritascenters` cc, `caritasbranch_has_caritascenters` cbc,
+							`caritasbranch` b
+							where `Members_ControlNo` = '$control_no' and cm.`CaritasCenters_ControlNo` = cc.`ControlNo` and
+							cbc.`CaritasCenters_ControlNo` = cc.`ControlNo` and
+							b.`ControlNo` = cbc.`CaritasBranch_ControlNo`");
+			return $branchcenter->result();
+	}
+	/*public function getcomaker($control_no){
+
+	}*/
+
+    
+}
+?>
--- application/views/general/terminate_voluntary.php
+++ application/views/general/terminate_voluntary.php
@@ -1,13 +1,160 @@
+<?php 
+		foreach ($profileinfo as $mem) {
+			$lastname = $mem->LastName;
+			$middlename = $mem->MiddleName;
+			$FirstName = $mem->FirstName;
+			$memid = $mem->MemberID;
+			$mfi = $mem->MFI;
+      		$enter = $mem->date;
+			$addresshome = $mem->Address;
+			$addressdate = $mem->AddressDate;
+			$contactno = $mem->ContactNo;
+			$bday = $mem->Birthday;
+			$bplace = $mem->BirthPlace;
+			$gender_id =$mem->GenderID;
+			$cstatus = $mem->CivilStatus;
+			$religion = $mem->Religion;
+			$btype = $mem->BusinessType;
+			$companyname = $mem->CompanyName;
+			$comcontact = $mem->CompanyContact;
+			$yearen = $mem->YearEntered;
+			$edu = $mem->EducationalAttainment;
+		}
+
+		foreach ($branchcenter as $bc) {
+			$branch = $bc->BranchName;
+			$center = $bc->CenterNo;
+		}
+
+ ?>
+
+
+Report for printing <br><br><br>
+PERSONAL INFORMATION <br><br>
+Name: <?php echo $lastname.",".$FirstName." ".$middlename; ?>
+<br>
+Member ID: <?php echo $memid; ?>
+<br>
+Home Address: <?php echo $addresshome; ?>
+<br>
+Contact Number: <?php echo $contactno; ?>
+<br>
+Branch: <?php echo $branch; ?>
+<br>
+Center: <?php echo $center; ?>
+<br>
+Date Entered: <?php echo $enter; ?>
+
+<br><br><br>
+
+
+CURRENT LOAN INFORMATION
+<br><br>
+Date Loan:
+<br>
+Loan Type:
+<br>
+Active Loan Release:
+<br>
+Loan Collected:
+<br>
+Remaining Balance:
+<br><br>
+
+	if wala current loan:   "NO CURRENT LOAN"
+
+<br><br>
+<!-- only shown if may current loan (BELOW) -->
+Co-MAKER INFORMATION
+<br>
 Name:
-
+<br>
 Member ID:
-
+<br>
 Home Address:
-
+<br>
 Contact Number:
+<br><br>
+<!-- only shown if may current loan (ABOVE) -->
 
-Branch:
-Center:
+SAVINGS, WITHDRAWAL & CAPITAL SHARE
+<br><br>
+Current Savings:
+<br>
+Savings Withdrawn:
+<br>
+Number of Capital Shares:
+<br>
+Capital Shares worth:
+<br><br><br>
 
-Date Entered:
+
+
+
+
+if may loan pa,  
+	"Please settle the remaining loan balance before termination."
+	have option: Pay remaining balance    Use Savings to pay   Cancel
+
+		if savings insufficient
+			"Savings Insufficient to pay remaining balance."
+
+			Current Savings:
+			Less: Loan Balance:
+			Remaining Loan Balance:  
+
+				option:  Pay remaining balance      Cancel
+
+				if Cancel
+					"Please settle the remaining loan balance before termination."
+				else
+					Loan Balance:
+					Payment Recieved:
+
+					Submit      cancel
+
+					Submit " You can no longer make changes after submitting this form. Are you sure you want to submit?"
+						ok - successfully paid remaining loan.
+
+
+		else
+			"Successfully paid loan through savings."
+
+			Current Savings:
+			Less: Loan Balance:
+			Total Savings Left:
+
+
+	PAY REMAINING BALANCE:
+
+		Loan Balance:
+		Payment Recieved:
+
+		Submit Cancel
+
+		Submit " You can no longer make changes after submitting this form. Are you sure you want to submit?"
+			ok - successfully paid remaining loan.
+
+
+IF NO LOAN BALANCE:
+are you sure you want to terminate this person?
+	ok    cancel
+
+IF OK
+	NAME:
+	Member ID:
+
+RETURNING BALANCE
+	Savings:
+	Capital Share:
+
+	TOTAL AMOUNT: 
+
+
+	BM Signature: BM				 Date:
+	Recieved By: the member          Date:
+	Issued By: SO/ person created the report  Date:
+
+	Released      Cancel
+
 
