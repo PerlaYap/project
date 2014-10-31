d597bfad75ab9fa42fe5f42caed86fbed409c27f
lecdado <lyka.dado@yahoo.com>
2014-10-31 13:54:44 +0800

    with Help

--- application/views/salveofficer/homepage.php
+++ application/views/salveofficer/homepage.php
@@ -435,7 +435,7 @@ function gotoapprovedloan(){
 
 		<p class="footertext">
 			&#169; 2014 Microfinance Information Management System <br>
-			<a href="#">ABOUT US</a> | <a href="#">CONTACT US</a> | <a href="#">FAQs</a>
+			<a href="#">ABOUT US</a> | <a href="#">CONTACT US</a> | <a href="#">FAQs</a> | <a href="#">HELP</a>
 		</p>
 
 		<br><br>
