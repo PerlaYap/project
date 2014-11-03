f11f3f7fbcba9962cb78c63c36259055aca265c6
lecdado <lyka.dado@yahoo.com>
2014-11-03 11:20:48 +0800

    fixed reports :>

--- application/views/reports/borrowerandsaver.php
+++ application/views/reports/borrowerandsaver.php
@@ -1,3 +1,14 @@
+<style type="text/css" media="print">
+  .dontprint{
+    display: none;
+  }
+
+  @page { 
+    size: Landscape;
+    margin: 1 in;
+  }
+</style>
+
 <?php 
 $n = $this->session->userdata('branchno');
 $user = $this->session->userdata('firstname');
@@ -115,6 +126,6 @@ date_default_timezone_set('Asia/Manila');
     </table>
     <br><br>
 	<div style="width: 100%; text-align: center;">
-		<button onclick="window.print()">Print</button> 
+		<button onclick="window.print()" class="dontprint">Print</button> 
 	</div>
 </body>
