329eb269ba1a6fa0fbf4d4767c4e4b6d387a2b04
killercl <lim.kendrick28@gmail.com>
2014-11-08 11:33:38 +0800

    ailln

--- application/views/navigation.php
+++ application/views/navigation.php
@@ -244,8 +244,8 @@ WHERE lhm.CaritasBranch_ControlNo= '$branch'");
 					<?php } else if($userrank=='branchmanager'){?>
 						<li class="menu"><a href="<?php echo site_url('reports/reportList'); ?>" class="menu" style="cursor: pointer;">REPORTS</a>
 							<ul>
-								<!--<li><a href="<?php echo site_url('reports/sbuandloan'); ?>">Savings Build-Up and Loan Report</a></li>-->
-								<li><a href="<?php echo site_url('reports/portfolioAtRisk'); ?>">Portfolio At Risk</a></li>
+								<!--<li><a href="<?php echo site_url('reports/sbuandloan'); ?>">Savings Build-Up and Loan Report</a></li>
+								<li><a href="<?php echo site_url('reports/portfolioAtRisk'); ?>">Portfolio At Risk</a></li>-->
 								<li><a href="<?php echo site_url('reports/centerperformance'); ?>">Collection Performance of Centers</a></li>
 								<li><a href="<?php echo site_url('reports/borrowerandsaver'); ?>">Borrower vs Saver</a></li>
 							</ul>
@@ -257,39 +257,6 @@ WHERE lhm.CaritasBranch_ControlNo= '$branch'");
 							</ul>
 						</li>
 					<?php } ?>
-
-
-
-
-					<li class="menu"><a href="<?php echo site_url('reports/reportList'); ?>" class="menu" style="cursor: pointer;">REPORTS</a>
-						<ul>
-							
-							<!-- <li><a href="<?php echo site_url('reports/monthlycollection'); ?>">Monthly Collection</a></li> -->
-							<!-- <li><a href="<?php echo site_url('reports/portfolioatrisk'); ?>">Portfolio At Risk</a></li> -->
-							<!-- <li><a href="<?php echo site_url('reports/revenueandcost'); ?>">Revenue and Financial Costs</a></li> -->
-							<?php if($userrank=='salveofficer') :?>
-							<!--<li><a href="<?php echo site_url('reports/dailycollectionsheet'); ?>">Daily Collection Sheet</a></li> -->
-							<?php endif;?>
-							<?php if($userrank=='branchmanager') :?>
-						<!--	<li><a href="<?php echo site_url('reports/sbuandloan'); ?>">Savings Build-Up and Loan Report</a></li> -->
-							<!-- <li><a href="<?php //echo site_url('reports/summaryreport'); ?>">Summary Report</a></li> -->
-						<!--	<li><a href="<?php echo site_url('reports/dailycollectionsheet'); ?>">Daily Collection Sheet</a></li> -->
-							<li><a href="<?php echo site_url('reports/centerperformance'); ?>">Collection Performance of Centers</a></li>
-							<li><a href="<?php echo site_url('reports/borrowerandsaver'); ?>">Borrower vs Saver</a></li>
-							<?php endif;?>
-							
-							<?php if($userrank=='mispersonnel') :?>
-							<li><a href="<?php echo site_url('reports/branchpastdue'); ?>">Collection Performance of Branches</a></li>
-							<!-- <li><a href="<?php // echo site_url('reports/dcsummary'); ?>">Daily Collection Summary</a></li> -->
-							<li><a href="<?php echo site_url('reports/mismonthly'); ?>">Monthly Account Report</a></li>
-							<li><a href="<?php echo site_url('reports/loanportfolio'); ?>">Monthly Loan Portfolio Report</a></li>
-							<!--<li><a href="<?php echo site_url('reports/pdmmonthly'); ?>">Monthly Past Due Mature Report</a></li>-->
-							<li><a href="<?php echo site_url('reports/sbushares'); ?>">Monthly Savings Build-Up and Capital Shares Report</a></li>
-							<?php endif;?>
-						</ul>
-					</li>
-					
-
 
 					<li class="menu"><a href="<?php echo site_url('records/recordlist'); ?>" class="menu" style="cursor: pointer;">RECORDS</a></li>
 				</ul>
