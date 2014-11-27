7b0d566f202d758ee5c2227690320bc856838ef0
killercl <lim.kendrick28@gmail.com>
2014-11-27 15:06:48 +0800

    Aging and Trends

--- application/views/reports/trend.php
+++ application/views/reports/trend.php
@@ -27,7 +27,7 @@
   $month = array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec");
 
  if($type==1){
- $lineBranch=$this->db->query("SELECT  Month(DateApplied) AS Month, ROUND((SUM(AmountRequested)/Count(ControlNo)),2) AS NumberofLoan FROM loanapplication la LEFT JOIN loanapplication_has_members lhm ON lhm.LoanApplication_ControlNo=la.ControlNo 
+ $lineBranch=$this->db->query("SELECT  Month(DateApplied) AS Month, ROUND(SUM(AmountRequested),2) AS NumberofLoan FROM loanapplication la LEFT JOIN loanapplication_has_members lhm ON lhm.LoanApplication_ControlNo=la.ControlNo 
 WHERE YEAR(DateApplied)='$years' AND (Status='Full Payment' OR Status='Current' OR Status='Active') AND CaritasBranch_ControlNo='$branch'
 GROUP BY Month(DateApplied)");
 
@@ -40,10 +40,10 @@ foreach($lineBranch->result() as $row){
   $arrayline[$row->Month]=$row->NumberofLoan;
 }}
 else if($type==2){
-$lineBranch1=$this->db->query("SELECT  Month(DateApplied) AS Month, ROUND((SUM(AmountRequested)/Count(ControlNo)),2) AS NumberofLoan FROM loanapplication la LEFT JOIN loanapplication_has_members lhm ON lhm.LoanApplication_ControlNo=la.ControlNo 
+$lineBranch1=$this->db->query("SELECT  Month(DateApplied) AS Month, ROUND(SUM(AmountRequested),2) AS NumberofLoan FROM loanapplication la LEFT JOIN loanapplication_has_members lhm ON lhm.LoanApplication_ControlNo=la.ControlNo 
 WHERE YEAR(DateApplied)='$years' AND (Status='Full Payment' OR Status='Current' OR Status='Active') AND CaritasBranch_ControlNo='$branch'
 GROUP BY Month(DateApplied)");
-$lineBranch2=$this->db->query("SELECT  Month(DateApplied) AS Month, ROUND((SUM(AmountRequested)/Count(ControlNo)),2) AS NumberofLoan FROM loanapplication la LEFT JOIN loanapplication_has_members lhm ON lhm.LoanApplication_ControlNo=la.ControlNo 
+$lineBranch2=$this->db->query("SELECT  Month(DateApplied) AS Month, ROUND(SUM(AmountRequested),2) AS NumberofLoan FROM loanapplication la LEFT JOIN loanapplication_has_members lhm ON lhm.LoanApplication_ControlNo=la.ControlNo 
 WHERE YEAR(DateApplied)='$years1' AND (Status='Full Payment' OR Status='Current' OR Status='Active') AND CaritasBranch_ControlNo='$branch'
 GROUP BY Month(DateApplied)");
 
