795a91cbde4523de326e276c1e5f740e2c29d70d
killercl <lim.kendrick28@gmail.com>
2014-11-27 08:08:44 +0800

    Commas In LoanApp2

--- application/views/salveofficer/addnewloan2.php
+++ application/views/salveofficer/addnewloan2.php
@@ -101,6 +101,84 @@ foreach ($dayoftheweek->result() as $row){
 ?>
 
 <script>
+function number_format(number, decimals, dec_point, thousands_sep) {
+  //  discuss at: http://phpjs.org/functions/number_format/
+  // original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: davook
+  // improved by: Brett Zamir (http://brett-zamir.me)
+  // improved by: Brett Zamir (http://brett-zamir.me)
+  // improved by: Theriault
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // bugfixed by: Michael White (http://getsprink.com)
+  // bugfixed by: Benjamin Lupton
+  // bugfixed by: Allan Jensen (http://www.winternet.no)
+  // bugfixed by: Howard Yeend
+  // bugfixed by: Diogo Resende
+  // bugfixed by: Rival
+  // bugfixed by: Brett Zamir (http://brett-zamir.me)
+  //  revised by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+  //  revised by: Luke Smith (http://lucassmith.name)
+  //    input by: Kheang Hok Chin (http://www.distantia.ca/)
+  //    input by: Jay Klehr
+  //    input by: Amir Habibi (http://www.residence-mixte.com/)
+  //    input by: Amirouche
+  //   example 1: number_format(1234.56);
+  //   returns 1: '1,235'
+  //   example 2: number_format(1234.56, 2, ',', ' ');
+  //   returns 2: '1 234,56'
+  //   example 3: number_format(1234.5678, 2, '.', '');
+  //   returns 3: '1234.57'
+  //   example 4: number_format(67, 2, ',', '.');
+  //   returns 4: '67,00'
+  //   example 5: number_format(1000);
+  //   returns 5: '1,000'
+  //   example 6: number_format(67.311, 2);
+  //   returns 6: '67.31'
+  //   example 7: number_format(1000.55, 1);
+  //   returns 7: '1,000.6'
+  //   example 8: number_format(67000, 5, ',', '.');
+  //   returns 8: '67.000,00000'
+  //   example 9: number_format(0.9, 0);
+  //   returns 9: '1'
+  //  example 10: number_format('1.20', 2);
+  //  returns 10: '1.20'
+  //  example 11: number_format('1.20', 4);
+  //  returns 11: '1.2000'
+  //  example 12: number_format('1.2000', 3);
+  //  returns 12: '1.200'
+  //  example 13: number_format('1 000,50', 2, '.', ' ');
+  //  returns 13: '100 050.00'
+  //  example 14: number_format(1e-8, 8, '.', '');
+  //  returns 14: '0.00000001'
+
+  number = (number + '')
+    .replace(/[^0-9+\-Ee.]/g, '');
+  var n = !isFinite(+number) ? 0 : +number,
+    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+    s = '',
+    toFixedFix = function(n, prec) {
+      var k = Math.pow(10, prec);
+      return '' + (Math.round(n * k) / k)
+        .toFixed(prec);
+    };
+  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n))
+    .split('.');
+  if (s[0].length > 3) {
+    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+  }
+  if ((s[1] || '')
+    .length < prec) {
+    s[1] = s[1] || '';
+    s[1] += new Array(prec - s[1].length + 1)
+      .join('0');
+  }
+  return s.join(dec);
+}
+
 	
 	$(document).ready(function(){
 		icalculateSum();
@@ -157,7 +235,7 @@ foreach ($dayoftheweek->result() as $row){
 		else
 			maxLoan=(savings/.40);
 	}
-		$("input#maxLoan").val(<?php echo number_format(maxLoan) ?>);
+		$("input#maxLoan").val(number_format(maxLoan,2));
 		
 		if(enteredLoan>maxLoan){
 			document.getElementById("loanamount").style.border="thin solid red";
@@ -177,7 +255,7 @@ foreach ($dayoftheweek->result() as $row){
 		}
 	});
 
-	$("input#isubtotal").val(sum.toFixed(2));
+	$("input#isubtotal").val(number_format(sum,2));
 	}
 
 	function fcalculateSum(){
@@ -189,7 +267,7 @@ foreach ($dayoftheweek->result() as $row){
 		}
 	});
 
-	$("input#fsubtotal").val(sum.toFixed(2));
+	$("input#fsubtotal").val(number_format(sum,2));
 	}
 
 	function bcalculateSum(){
@@ -201,7 +279,7 @@ foreach ($dayoftheweek->result() as $row){
 		}
 	});
 
-	$("input#bsubtotal").val(sum.toFixed(2));
+	$("input#bsubtotal").val(number_format(sum,2));
 	}
 
 	function materialSum(){
@@ -227,7 +305,7 @@ foreach ($dayoftheweek->result() as $row){
 		}
 	}
 
-	$("input#materialtotal").val(sum.toFixed(2));
+	$("input#materialtotal").val(number_format(sum));
 	}
 
 	function total(){
@@ -235,15 +313,15 @@ foreach ($dayoftheweek->result() as $row){
 		var expense= 0;
 		var difference=0;
 
-			income+=parseFloat($("input#isubtotal").val());
+			income+=parseFloat($("input#isubtotal").val().replace(',',''));
 		
-			expense+=parseFloat($("input#bsubtotal").val()) + parseFloat($("input#fsubtotal").val());
+			expense+=parseFloat($("input#bsubtotal").val()) + parseFloat($("input#fsubtotal").val().replace(',',''));
 
 			difference+=parseFloat(income)-parseFloat(expense);
 
-	$("input#incometotal").val(income.toFixed(2));
-	$("input#expensetotal").val(expense.toFixed(2));
-	$("input#difference").val(difference.toFixed(2));
+	$("input#incometotal").val(number_format(income,2));
+	$("input#expensetotal").val(number_format(expense,2));
+	$("input#difference").val(number_format(difference,2));
 	}
 
 	(function($) {
@@ -251,7 +329,7 @@ foreach ($dayoftheweek->result() as $row){
             this.each( function( i ) {
                 $(this).change( function( e ){
                     if( isNaN( parseFloat( this.value ) ) ) return;
-                    this.value = parseFloat(this.value).toFixed(2);
+                    this.value = number_format(this.value,2);
                 });
             });
             return this; //for chaining
